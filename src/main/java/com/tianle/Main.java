4cdf189ef11f47771acb4ffa6bedddfe308f981b
tianle <tianle@qq.com>
2021-09-28 03:17:17 +0800

    first commit

---
+++ src/main/java/com/tianle/Main.java
@@ -0,0 +1,14 @@
+package com.tianle;
+
+/**
+ * @author ：tianLe
+ * @date ：Created in 2021/9/28 3:02 上午
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
