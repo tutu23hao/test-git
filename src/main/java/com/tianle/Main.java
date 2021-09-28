bb9fa1fd45c112c78361461aa530baee97f919a8
tianle <tianle@qq.com>
2021-09-28 13:06:03 +0800

    hotfix-2 commit

--- src/main/java/com/tianle/Main.java
+++ src/main/java/com/tianle/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("hello3333333311");
         System.out.println("hotfix");
         System.out.println("");
+        System.out.println("hotfix");
     }
 }
