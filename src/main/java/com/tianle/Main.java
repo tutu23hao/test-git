728a0485ea26ca6c98f43a5e76fd9726c05c0726
tianle <tianle@qq.com>
2021-09-28 14:46:10 +0800

    push commit

--- src/main/java/com/tianle/Main.java
+++ src/main/java/com/tianle/Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println("master");
         System.out.println("pushTest");
              System.out.println("pullTest");
+        System.out.println("tianle");
 
     }
 }
