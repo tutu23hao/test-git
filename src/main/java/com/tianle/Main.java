c82982b6dce27c06fed22cd0dc6db9806d924a7a
tianyue <1090532240@qq.com>
2022-03-12 09:05:33 +0800

    添加controller hello 组件

--- src/main/java/com/tianle/Main.java
+++ src/main/java/com/tianle/Main.java
@@ -1,26 +1,16 @@
 package com.tianle;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * @author ：tianLe
- * @date ：Created in 2021/9/28 3:02 上午
- * @description：
- * @modified By：
- * @version:
+ *
  */
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("helloWorld");
-        System.out.println("hello111111");
-        System.out.println("hello3333333311");
-        System.out.println("hotfix");
-        System.out.println("");
-        System.out.println("hotfix");
-        System.out.println("master");
-        System.out.println("pushTest");
-        System.out.println("pullTest");
-        System.out.println("tianle");
-        System.out.println("233444");
-        
+
+        SpringApplication.run(Main.class,args);
         
 
     }
---
+++ src/main/java/com/tianle/controller/FirstController.java
@@ -0,0 +1,20 @@
+package com.tianle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：tianLe
+ * @date ：Created in 2022/3/12 8:54 上午
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@RestController
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "helloWorld!!!!!";
+    }
+}
