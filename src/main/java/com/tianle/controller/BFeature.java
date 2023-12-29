4613808ad984213567a3a830b27d2f45422ae3de
tianLe <1090532240@qq.com>
2023-12-29 16:36:02 +0800

    feature-c

---
+++ src/main/java/com/tianle/controller/BFeature.java
@@ -0,0 +1,15 @@
+package com.tianle.controller;
+
+import lombok.Data;
+
+/**
+ * @author ：tianLe
+ */
+@Data
+public class BFeature {
+
+
+    private String userName;
+
+    private String mail;
+}
